e80efffafd188ccde00f8a656f9c00763d80c233
liang <guoliang1222@gmail.com>
2017-02-14 23:38:55 +0200

    update config.extURL and update ui side _self link

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const config = {
 		port: process.env.PORT || 9080,
 		basePath: process.env.API_SERVER_BASEPATH  || '/api/',
 		secrets: [process.env.API_SERVER_SECRET || 'whatever'],
-		extURL: process.env.API_SERVER_EXTURL || 'http://chat.loveyoung.me:9080/api/v1'
+		extURL: process.env.API_SERVER_EXTURL || 'http://chat.loveyoung.me/api/v1'
 	},
 	mongo: {
 		url: process.env.API_SERVER_MONGODB_URL || 'mongodb://localhost:27017/chat'
